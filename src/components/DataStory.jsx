6cd20dafea139cc8137dfaa5ee26ed22ef0aa60b
AaronFurlan <aaron.furlan@outlook.com>
2024-01-02 19:41:09 +0100

    Added text to the pace by event and winning time progression plot.

--- src/components/DataStory.jsx
+++ src/components/DataStory.jsx
@@ -36,23 +36,25 @@ export default function DataStory() {
   return (
     <>
       <Box className="container">
-        <Typography variant="h6" component="p" sx={{ mb: 2 }}>
-          What is the expected age of peak performance for runners? And does
-          this vary by event? Let's take a look at the age distribution of the
-          top 1000 runners in each event. It is generally accepted that peak
-          performance for athletes is around 25 years old, but is this actually
-          supported by the data?
-          <Tooltip title={marathonhandbook}>
-            <Link
-              href={marathonhandbook}
-              target="_blank"
-              rel="noopener noreferrer"
-              sx={{ ml: "5px" }}
-            >
-              <sup>[1]</sup>
-            </Link>
-          </Tooltip>
-        </Typography>
+        <Box className="row">
+          <Typography variant="h6" component="p" sx={{ mb: 2 }}>
+            What is the expected age of peak performance for runners? And does
+            this vary by event? Let's take a look at the age distribution of the
+            top 1000 runners in each event. It is generally accepted that peak
+            performance for athletes is around 25 years old, but is this actually
+            supported by the data?
+            <Tooltip title={marathonhandbook}>
+              <Link
+                href={marathonhandbook}
+                target="_blank"
+                rel="noopener noreferrer"
+                sx={{ ml: "5px" }}
+              >
+                <sup>[1]</sup>
+              </Link>
+            </Tooltip>
+          </Typography>
+        </Box>
         <Box className="row">
           <Typography variant="h6" component="p" className="text">
             For the sprint events (100m, 200m, 400m), we see that the peak age
@@ -116,11 +118,11 @@ export default function DataStory() {
             component="img"
             src={paceByEventImage}
             alt="Pace by event"
-            className="image"
+            className="rightImage"
             onClick={() => openLightbox(3)}
             sx={{ cursor: "pointer" }}
           />
-          <Typography variant="h6" component="p" className="text">
+          <Typography variant="h6" component="p" className="text" style={{alignSelf: 'flex-start'}}>
             Athletic performance hinges on two energy systems: anaerobic for
             short, intense activities like sprints, and aerobic for longer,
             endurance events. The anaerobic system quickly delivers energy
@@ -130,30 +132,45 @@ export default function DataStory() {
             explaining why long-distance athletes are often older. Their
             prolonged training culminates in a robust aerobic system, suited for
             the demands of endurance events.
+            <br/>
+            <br/>
+            The challenge in the 800m run is to balance these systems. Athletes need to start fast enough to be competitive but must conserve enough energy to maintain a strong pace and finish with a powerful sprint. This balancing act between speed and endurance, and the switch between energy systems, makes the 800m particularly demanding.
+
           </Typography>
         </Box>
         <Box className="row">
-          <Typography variant="h6" component="p" className="text">
-            The winning times for the marathon have been steadily decreasing
-            over the years. This is likely due to a combination of factors
-            including better training, improved nutrition, and technological
-            advancements in equipment.
-          </Typography>
           <Box
             component="img"
             src={winningTimesProgressionImage}
             alt="Winning Times Progression"
-            className="image"
+            className="rightImage"
             onClick={() => openLightbox(4)}
             sx={{ cursor: "pointer" }}
           />
+          <Typography variant="h6" component="p" className="text" style={{alignSelf: 'flex-start'}}>
+            Another interesting chapter is the development of winning times over the years. The plots show the
+            developments of winning times with the help of regression lines.
+            <br/>
+            <br/>
+            The most noticeable trend is seen in the women's 400m event. This is the only discipline where the winning
+            times over the years have actually become slower on average, in contrast to the trend of all other
+            disciplines. This can have various reasons. Among other things, performance-enhancing drugs were often used
+            between 1970 and 1990, especially among women.
+            <br/>
+            <br/>
+            For almost all other events, there is a clear trend of decreasing winning times, which shows the general
+            development in athletics, possibly due to advances in training, nutrition, and technological advancements in
+            equipment.
+            <br/>
+            <br/>
+            In the 10,000m run, one can see that data for women were only recorded from around 1980. In fact, the first officially recognized
+            10,000m run for women was in 1981.
+          </Typography>
         </Box>
         <Box className="row">
           <Typography
-            variant="h6"
-            component="p"
-            className="text"
-            sx={{ width: 50 }}
+            variant="h6" component="p" className="text"
+            sx={{ width: 700 }}
           >
             During the Cold War era, the USA and Russia emerged as formidable
             rivals in global athletics. This rivalry was marked by intense
